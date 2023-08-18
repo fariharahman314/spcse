74b59a6caf27ed4e5469a40c293b5672b6117c9c
fariharahman314 <139142978+fariharahman314@users.noreply.github.com>
2023-08-18 21:53:50 +0600

    Add files via upload

---
+++ cse printing100.c
@@ -0,0 +1,13 @@
+//code for printing cse 100 times//
+/*
+#include<stdio.h>
+int main()
+{
+    int i;
+    int n=100;
+    for(i=1; i<=n; i++)
+    {
+        printf(" CSE ",i);
+    }
+    return 0;
+}

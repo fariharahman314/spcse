358b1e9cd442d34407558b734e34f8302377ede3
fariharahman314 <139142978+fariharahman314@users.noreply.github.com>
2023-08-18 21:47:09 +0600

    Add files via upload

---
+++ upside down pyramid.c
@@ -0,0 +1,24 @@
+//code for upside down pyramid//
+/*
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1; i<=5; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=1; i<=5; i++)
+    {
+        for(j=4; j>=i; j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}*/

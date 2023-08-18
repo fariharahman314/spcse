a18b0fb6496926401a853579e37ce5fb678ebfd5
fariharahman314 <139142978+fariharahman314@users.noreply.github.com>
2023-08-18 21:55:08 +0600

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void swap (int a,int b){
+int temp=a;
+a=b;
+b=temp;
+printf("After swapping a=%d,b=%d\n",a,b);
+}
+int main(){int a,b;
+scanf("%d%d",&a,&b);
+swap(a,b);
+return 0;
+}*//*
